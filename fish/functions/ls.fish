6d605b621208d2dcec09b463d81ce3c343f54a83
Niklas Schönberg <niklas@uplink.fi>
2023-11-09 19:12:02 +0200

    Wezterm starship fish

---
+++ fish/functions/ls.fish
@@ -0,0 +1,4 @@
+function ls --wraps='eza --icons --group-directories-first --git' --description 'alias ls eza --icons --group-directories-first --git'
+  eza --icons --group-directories-first --git $argv
+        
+end
---
+++ wezterm/wezterm.lua
@@ -0,0 +1,48 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+
+config.color_scheme = 'Dark Pastel'
+config.window_background_opacity = 0.6
+config.hide_tab_bar_if_only_one_tab = true
+config.enable_scroll_bar = true
+
+config.window_background_gradient = {
+  orientation = 'Vertical',
+
+  colors = {
+    '#3e2222',
+    '#393939',
+  },
+
+  interpolation = 'Linear',
+
+  blend = 'Rgb',
+}
+
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+config.inactive_pane_hsb = {
+  saturation = 0.9,
+  brightness = 0.8,
+}
+
+config.font = wezterm.font 'Fira Mono Nerd Font'
+
+-- and finally, return the configuration to wezterm
+return config
